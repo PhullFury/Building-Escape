54d9bbdc6c9f841d92f4d3b3f57fa921136af30c
PhullFury <66832169+PhullFury@users.noreply.github.com>
2020-07-11 20:58:12 +0530

    Added a way to get player's viewpoint, also added a debug line to check player's "reach".

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "Grabber.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -19,8 +23,7 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("Grabber ready for duty!"));	
 }
 
 
@@ -29,6 +32,36 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	//Get Player's viewpiont
+	FVector PlayerViewPointLocation;
+	FRotator PlayerVeiwPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerVeiwPointRotation
+	);
+
+	//Logging out to test
+	FString PlayerLocation = PlayerViewPointLocation.ToString();
+	FString PlayerRotation = PlayerVeiwPointRotation.ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerLocation, *PlayerRotation);
+
+	//Draw a line from player showing reach
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerVeiwPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+
+	//Ray Cast out to a certain distance (Reach)
+	//See what it hits
 }
 
